697f7649208251ea30b24a0646e45ce22111d426
Jeremy Douglass <jeremydouglass@gmail.com>
2019-12-12 01:42:16 -0800

    release.sh -- documentation

--- release.sh
+++ release.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 zip -r ../processing_rosetta_examples.zip . -x "*.git*" -x "*.DS_Store" -x "*.sh"
+
+# In order to release
+# 
+# 1. check out a clean master (with no pending files)
+# 2. update the version number in processing_rosetta_examples.txt
+# 3. run the release script in the repo root -- this generates the zip outside
+# 4. on gihub, create a new release tagged e.g. v0.4
+# 5. add two assets as attachments:
+#    -  processing_rosetta_examples.txt
+#    -  processing_rosetta_examples.zip
+# 
+# Done! The "latest" path will automatically point to the newest release
+# and its download assets -- this will update what PDE displays and loads.
