063844fc371592ea347f6a736ca2cf37cab5308e
Jeremy Douglass <jeremydouglass@gmail.com>
2019-12-15 09:35:24 -0800

    release.sh -- build release zip with a named top-level directory

--- release.sh
+++ release.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-mkdir -p release
-rm release/processing_rosetta_examples.zip
-zip -r release/processing_rosetta_examples.zip . -x "*.git*" -x "release*" -x "*logo*" -x "*.DS_Store" -x "*.sh"
-cp examples.properties release/processing_rosetta_examples.txt
+RELD=release
+ZIPD=processing_rosetta_examples
+
+if [ -d $RELD/$ZIPD ]; then rm -Rf $RELD/$ZIPD; fi
+mkdir -p $RELD/$ZIPD
+
+cp examples.properties $RELD/$ZIPD.txt
+
+cp -r examples $RELD/$ZIPD/examples
+cp CONTRIBUTING.md $RELD/$ZIPD/
+cp examples.properties $RELD/$ZIPD/
+cp LICENSE.md $RELD/$ZIPD/
+cp README.md $RELD/$ZIPD/
+
+cd $RELD
+if [ -f $ZIPD.zip ]; then rm $ZIPD.zip; fi
+zip -r $ZIPD.zip $ZIPD -x "*.DS_Store" "*.git*"
+rm -r $ZIPD
 
 # In order to release
 # 
 # 1. check out a clean master (with no pending files)
-# 2. update the version number in processing_rosetta_examples.txt
+# 2. update the version number in examples.properties
 # 3. run the release script in the repo root -- this generates release files 
-# 4. on gihub, create a new release tagged e.g. v0.4
-# 5. add two release/ assets as attachments:
+# 4. on gihub, create a new release tagged with the version, e.g. v0.4
+# 5. add two release/ assets as attachments to the GitHub release:
 #    -  processing_rosetta_examples.txt
 #    -  processing_rosetta_examples.zip
 # 
